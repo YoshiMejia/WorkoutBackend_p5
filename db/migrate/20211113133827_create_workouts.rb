a325ea75f3aa1eccd807a4390a49e31b84c92a7f
Yoshi <yoshimejia00@gmail.com>
2021-11-13 08:58:42 -0500

    added description attribute to workoouts

--- db/migrate/20211113133827_create_workouts.rb
+++ db/migrate/20211113133827_create_workouts.rb
@@ -2,6 +2,7 @@ class CreateWorkouts < ActiveRecord::Migration[6.1]
   def change
     create_table :workouts do |t|
       t.string :name
+      t.string :description
       t.integer :reps
       t.integer :sets
       t.integer :completed
