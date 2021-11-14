eacf35288c7381010515d9df19fd3f1da7201f5d
Yoshi <yoshimejia00@gmail.com>
2021-11-14 17:21:54 -0500

    removed 'completed' attribute from seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 
 cardioDay = Planner.create(name:'cardio day')
 
-pushups = Workout.create(name:'pushups', description:'hands on floor, push', sets: 3, reps: 10, completed: 0, planner: cardioDay)
-jumpingJacks = Workout.create(name:'jumping jacks', description:'jump and wave arms around', sets: 3, reps: 20, completed: 0, planner: cardioDay)
-jog = Workout.create(name:'jog', description:'run for 10 minutes', sets: 3, reps: 1, completed: 0, planner: cardioDay)
-crunches = Workout.create(name:'crunches', description:'lay on back and bring knees to chest', sets: 3, reps: 15, completed: 0, planner: cardioDay)
-abExtensions = Workout.create(name:'abdominal extensions', description:'lay on back, knees to chest and then push them back out', sets: 3, reps: 15, completed: 0, planner: cardioDay)
-romanian = Workout.create(name:'romanian deadlift', description:'bend over and pick up barbel', sets: 4, reps: 8, completed: 0, planner: cardioDay)
+pushups = Workout.create(name:'pushups', description:'hands on floor, push', sets: 3, reps: 10, planner: cardioDay)
+jumpingJacks = Workout.create(name:'jumping jacks', description:'jump and wave arms around', sets: 3, reps: 20, planner: cardioDay)
+# jog = Workout.create(name:'jog', description:'run for 10 minutes', sets: 3, reps: 1, planner: cardioDay)
+# crunches = Workout.create(name:'crunches', description:'lay on back and bring knees to chest', sets: 3, reps: 15, planner: cardioDay)
+# abExtensions = Workout.create(name:'abdominal extensions', description:'lay on back, knees to chest and then push them back out', sets: 3, reps: 15, planner: cardioDay)
+# romanian = Workout.create(name:'romanian deadlift', description:'bend over and pick up barbel', sets: 4, reps: 8, planner: cardioDay)
 
