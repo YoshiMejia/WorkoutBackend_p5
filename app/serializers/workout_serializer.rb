ca0585be979867bfe1443f735e0c3287e188e93b
Yoshi <yoshimejia00@gmail.com>
2021-11-14 17:21:39 -0500

    removed 'completed' attribute from serializer

--- app/serializers/workout_serializer.rb
+++ app/serializers/workout_serializer.rb
@@ -1,4 +1,4 @@
 class WorkoutSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :sets, :reps, :completed, :planner_id
+  attributes :id, :name, :description, :sets, :reps, :planner_id
   belongs_to :planner
 end
