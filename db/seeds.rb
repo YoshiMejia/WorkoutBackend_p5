fd06d8e39c0f5168b31f10a801ce95590378082d
Yoshi <yoshimejia00@gmail.com>
2021-11-18 09:56:11 -0500

    reformatted seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 
 cardioDay = Planner.create(name:'cardio day')
 
-pushups = Workout.create(name:'pushups', description:'hands on floor, push', sets: 3, reps: 10, planner: cardioDay)
-jumpingJacks = Workout.create(name:'jumping jacks', description:'jump and wave arms around', sets: 3, reps: 20, planner: cardioDay)
-jog = Workout.create(name:'jog', description:'run for 10 minutes', sets: 3, reps: 1, planner: cardioDay)
-crunches = Workout.create(name:'crunches', description:'lay on back and bring knees to chest', sets: 3, reps: 15, planner: cardioDay)
-abExtensions = Workout.create(name:'abdominal extensions', description:'lay on back, knees to chest and then push them back out', sets: 3, reps: 15, planner: cardioDay)
-romanian = Workout.create(name:'romanian deadlift', description:'bend over and pick up barbel', sets: 4, reps: 8, planner: cardioDay)
+pushups = Workout.create(name:'Pushups', description:'Hands on floor, push', sets: 3, reps: 10, planner: cardioDay)
+jumpingJacks = Workout.create(name:'Jumping Jacks', description:'Jump and wave arms around', sets: 3, reps: 20, planner: cardioDay)
+jog = Workout.create(name:'Jog', description:'Run for 10 minutes', sets: 3, reps: 1, planner: cardioDay)
+crunches = Workout.create(name:'Crunches', description:'Lay on back and bring knees to chest', sets: 3, reps: 15, planner: cardioDay)
+abExtensions = Workout.create(name:'Abdominal Extensions', description:'Lay on back, knees to chest and then push them back out', sets: 3, reps: 15, planner: cardioDay)
+romanian = Workout.create(name:'Romanian Deadlift', description:'Bend over and pick up barbel', sets: 4, reps: 8, planner: cardioDay)
 
